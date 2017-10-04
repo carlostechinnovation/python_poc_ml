406ea2be3f58c2c27dbe452aee6a0f87c0639f29
root <carlosandresgarcia1986@hotmail.com>
2017-10-04 22:57:01 +0200

    Primer codigo

--- poc_mysql.py
+++ poc_mysql.py
@@ -1,18 +1,27 @@
 import mysql.connector
-from mysql.connector import errorcode
 
 
+def funcionBaseDatos():
+    print "POC- Conexion a Base de datos"
+    con = mysql.connector.connect(user='root', password='datos1986',host='127.0.0.1',database='datos_desa')
+    c = con.cursor()
 
-cnx = mysql.connector.connect(user='root', password='datos1986',
-                                 host='127.0.0.1',
-                                 database='datos_desa')
+    c.execute("""CREATE TABLE datos_desa.tb_poc_bbdd (campo1 int);""")
+    c.execute("""INSERT INTO datos_desa.tb_poc_bbdd (campo1) VALUES(1),(2),(3);""")
+    c.execute("""SELECT campo1 FROM datos_desa.tb_poc_bbdd LIMIT 5;""")
+    filas = []
+    for row in c:
+        user = {
+            'valor1': row[0]
+        }
+        print(row[0])
+        filas.append(user)
 
-except err = mysql.connector.Error:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
-    else:
-        print(err)
-else:
-    cnx.close()
+    c.execute("""DROP TABLE datos_desa.tb_poc_bbdd;""")
+    c.close()
+    return filas
+
+
+print "INICIO"
+funcionBaseDatos()
+print "FIN"
