4f20f067b0382f7843d2812c8ae1971f2446f593
root <carlosandresgarcia1986@hotmail.com>
2018-01-27 21:33:06 +0100

    Version inicial 1.2 20180127

--- galgos/galgos_regresion_train_test.py
+++ galgos/galgos_regresion_train_test.py
@@ -15,6 +15,8 @@ print("Entrada: PASADO --> features y targets, de train y test.")
 print("****************")
 
 
+################# FUNCIONES #################
+
 def leerDataset(camposRelleno, sufijoTiempo, sufijoTipoDs, sufijoGrupoColumnas, sufijoEtiqueta):
     c = pymysql.connect(host='127.0.0.1', user='root', passwd='datos1986', db='datos_desa').cursor()
     consulta = "SELECT *" + camposRelleno + " FROM datos_desa.tb_ds" + sufijoTiempo + sufijoTipoDs + sufijoGrupoColumnas + sufijoEtiqueta + ";"
@@ -28,7 +30,7 @@ def leerDataset(camposRelleno, sufijoTiempo, sufijoTipoDs, sufijoGrupoColumnas, 
         "dataset" + sufijoTiempo + sufijoTipoDs + sufijoGrupoColumnas + sufijoEtiqueta + "-Filas = " + str(len(alist)))
     print("dataset" + sufijoTiempo + sufijoTipoDs + sufijoGrupoColumnas + sufijoEtiqueta + "-Shape = " + str(
         dataOut.shape[0]) + "x" + str(dataOut.shape[1]))
-    print("dataset" + sufijoTiempo + sufijoTipoDs + sufijoGrupoColumnas + sufijoEtiqueta + "-Ejemplo --> " + str(
+    print("dataset" + sufijoTiempo + sufijoTipoDs + sufijoGrupoColumnas + sufijoEtiqueta + "-Ejemplo:\n" + str(
         alist[0]))
     return dataOut
 
@@ -45,6 +47,8 @@ X_train_features = leerDataset(", 0 AS relleno", "_pasado", "_train", "_features
 X_train_targets = leerDataset("", "_pasado", "_train", "_targets", sufijoEtiqueta)
 X_test_features = leerDataset(", 0 AS relleno", "_pasado", "_test", "_features", sufijoEtiqueta)
 X_test_targets = leerDataset("", "_pasado", "_test", "_targets", sufijoEtiqueta)
+X_validation_features = leerDataset(", 0 AS relleno", "_pasado", "_validation", "_features", sufijoEtiqueta)
+X_validation_targets = leerDataset("", "_pasado", "_validation", "_targets", sufijoEtiqueta)
 
 ############################################
 print(
@@ -54,35 +58,46 @@ imp = Imputer(missing_values='NaN', strategy='median', axis=0)
 # Numero de filas (no columnas) de cada dataset
 num_train = X_train_features.shape[0]
 num_test = X_test_features.shape[0]
+num_validation = X_validation_features.shape[0]
 
-# Juntar TRAIN+TEST
-X_trainYtest_features = np.concatenate((X_train_features, X_test_features), axis=0)
-print("\nShape (train+test SIN padding, pero con columna de RELLENO que la va a quitar el Imputer) = " + str(
-    X_trainYtest_features.shape[0]) + "x" + str(X_trainYtest_features.shape[1]))
+# Juntar TRAIN+TEST+VALIDATION
+X_juntos_features_aux = np.concatenate((X_train_features, X_test_features), axis=0)
+X_juntos_features = np.concatenate((X_juntos_features_aux, X_validation_features), axis=0)
+print("\nShape (JUNTOS SIN padding, pero con columna de RELLENO que la va a quitar el Imputer) = "
+      + str(X_juntos_features.shape[0]) + "x" + str(X_juntos_features.shape[1]))
 
 # Rellenar los NULL con el valor de la MEDIANA de cada columna
-X_trainYtest_features_sinnulos = imp.fit(X_trainYtest_features).transform(X_trainYtest_features)
-print("\nShape (train+test CON padding) = " + str(X_trainYtest_features_sinnulos.shape[0]) + "x" + str(
-    X_trainYtest_features_sinnulos.shape[1]))
-print("Primera fila de X_conpadding: " + str(X_trainYtest_features_sinnulos[0]))
+X_juntos_features_sinnulos = imp.fit(X_juntos_features).transform(X_juntos_features)
+print("\nShape (juntos CON padding) = " + str(X_juntos_features_sinnulos.shape[0]) + "x" + str(
+    X_juntos_features_sinnulos.shape[1]))
 
 # Separar otra vez las FEATURES de TRAIN y TEST, pero ya tienen hecho el relleno
-X_train_features_sinnulos = X_trainYtest_features_sinnulos[0:int(num_train)]  # el ultimo indice es excluyente
+X_train_features_sinnulos = X_juntos_features_sinnulos[0:int(num_train)]  # el ultimo indice es excluyente
 print("\nShape (train CON padding) = " + str(X_train_features_sinnulos.shape[0]) + "x" + str(
     X_train_features_sinnulos.shape[1]))
 print("Primera fila de X_train_features_sinnulos: " + str(X_train_features_sinnulos[0]))
 
-X_test_features_sinnulos = X_trainYtest_features_sinnulos[
+X_test_features_sinnulos = X_juntos_features_sinnulos[
                            int(num_train):(num_train + num_test)]  # el ultimo indice es excluyente
 print("\nShape (test CON padding) = " + str(X_test_features_sinnulos.shape[0]) + "x" + str(
     X_test_features_sinnulos.shape[1]))
 print("Primera fila de X_test_features_sinnulos: " + str(X_test_features_sinnulos[0]))
+
+X_validation_features_sinnulos = X_juntos_features_sinnulos[
+                                 int(num_train + num_test):(
+                                     num_train + num_test + num_validation)]  # el ultimo indice es excluyente
+print("\nShape (X_validation_features_sinnulos) = " + str(X_validation_features_sinnulos.shape[0]) + "x" + str(
+    X_validation_features_sinnulos.shape[1]))
+print("Primera fila de X_test_features_sinnulos: " + str(X_validation_features_sinnulos))
+
 
 ##################### Datasets preparados para usar ###########
 # X_train_features_sinnulos
 # X_train_targets
 # X_test_features_sinnulos
 # X_test_targets
+# X_validation_features_sinnulos
+# X_validation_targets
 
 ################### Comprobacion de: Input contains NaN, infinity or a value too large for dtype('float64'). ########
 print("checkInvalidos_train_features_sinnulos --> ")
@@ -95,7 +110,7 @@ checkInvalidos_test_features_sinnulos = np.isnan(X_test_features_sinnulos)
 # checkInvalidos_X_test_targets=np.isnan(X_test_targets)
 
 #####################################
-print("\n\nMACHINE LEARNING: REGRESIONES")
+print("\n*********************\nMACHINE LEARNING: REGRESIONES\n*********************\n")
 #####################################
 
 ########### Lasso ##################################################
@@ -106,7 +121,8 @@ modeloLasso = Lasso(alpha=0.1, copy_X=True, fit_intercept=True, max_iter=1000,
                     selection='cyclic', tol=0.0001, warm_start=False)
 print(modeloLasso)
 
-y_pred_lasso = modeloLasso.fit(X_train_features_sinnulos, X_train_targets).predict(X_test_features_sinnulos)
+modeloLassoEntrenado = modeloLasso.fit(X_train_features_sinnulos, X_train_targets)
+y_pred_lasso = modeloLassoEntrenado.predict(X_test_features_sinnulos)
 r2_score_lasso = r2_score(X_test_targets, y_pred_lasso)
 
 print("\n\nREGRESION - Lasso --> R^2 (sobre DS-test) = %f" % r2_score_lasso)
@@ -116,32 +132,66 @@ print('\n\n')
 # ElasticNet
 from sklearn.linear_model import ElasticNet
 
-modeloElasticNet = ElasticNet(alpha=0.1, l1_ratio=0.7)
+modeloElasticNet = ElasticNet(alpha=0.1, copy_X=True, fit_intercept=True, l1_ratio=0.7,
+                              max_iter=1000, normalize=False, positive=False, precompute=False,
+                              random_state=None, selection='cyclic', tol=0.0001, warm_start=False)
 
-y_pred_enet = modeloElasticNet.fit(X_train_features_sinnulos, X_train_targets).predict(X_test_features_sinnulos)
-r2_score_elasticnet = r2_score(X_test_targets, y_pred_enet)
 print(modeloElasticNet)
+
+modeloElasticNetEntrenado = modeloElasticNet.fit(X_train_features_sinnulos, X_train_targets)
+y_pred_enet = modeloElasticNetEntrenado.predict(X_test_features_sinnulos)
+r2_score_elasticnet = r2_score(X_test_targets, y_pred_enet)
+
 print("\n\nREGRESION - ElasticNet --> R^2 (sobre DS-test) = %f" % r2_score_elasticnet)
 print('\n\n')
 
-#############################################
 
+#######################################################
+# MODELO + Prediccion sobre DATASET-PASADO-VALIDATION
+#######################################################
+# X_validation_features_sinnulos
+# X_validation_targets
+
+def predecirValidationTargets(modelo, matriz_features, sufijoEtiqueta):
+    # PREDICCION de los targets sobre DS-pasado-validation
+    print("Prediciendo targets de validation...")
+    validation_targets_path = "/home/carloslinux/Desktop/DATOS_LIMPIO/galgos/pasado_validation_targets_predichos" + sufijoEtiqueta + ".txt"
+    print("path --> " + validation_targets_path)
+    pasado_validation_targets_predichos = modelo.predict(matriz_features)
+    print("pasado_validation_targets_predichos:\n")
+    print(pasado_validation_targets_predichos)
+    print("Longitud de salida targets_validation (PREDICHOS) =" + str(len(pasado_validation_targets_predichos)))
+    print("Primera fila de pasado_validation_targets_predichos: " + str(pasado_validation_targets_predichos[0]))
+
+    fichero_resultados = open(validation_targets_path, 'w')
+    for item in pasado_validation_targets_predichos:
+        fichero_resultados.write("%s\n" % item)
+
+    return pasado_validation_targets_predichos
+
+
+##############################################
 ############################################
 
 print("Guardando modelos...")
-modeloGuardado = joblib.dump(modeloLasso,
+joblib.dump(modeloLassoEntrenado,
                              '/home/carloslinux/Desktop/GIT_REPO_PYTHON_POC_ML/python_poc_ml/galgos/galgos_modeloLasso.pkl')
-modeloGuardado = joblib.dump(modeloElasticNet,
+joblib.dump(modeloElasticNetEntrenado,
                              '/home/carloslinux/Desktop/GIT_REPO_PYTHON_POC_ML/python_poc_ml/galgos/galgos_elasticnet.pkl')
+
 
 print("Guardando modelo GANADOR...")
 if (r2_score_lasso >= r2_score_elasticnet):
     print("Gana modelo Lasso")
-    modeloGuardado = joblib.dump(modeloLasso,
+    joblib.dump(modeloLassoEntrenado,
                                  '/home/carloslinux/Desktop/GIT_REPO_PYTHON_POC_ML/python_poc_ml/galgos/galgos_regresion_MEJOR_MODELO.pkl')
+    predecirValidationTargets(modeloLassoEntrenado, X_validation_features_sinnulos, sufijoEtiqueta)
 else:
     print("Gana modelo ElasticNet")
-    modeloGuardado = joblib.dump(modeloElasticNet,
+    joblib.dump(modeloElasticNetEntrenado,
                                  '/home/carloslinux/Desktop/GIT_REPO_PYTHON_POC_ML/python_poc_ml/galgos/galgos_regresion_MEJOR_MODELO.pkl')
+    predecirValidationTargets(modeloElasticNetEntrenado, X_validation_features_sinnulos, sufijoEtiqueta)
+
+
 
 print("FIN")
